1bd884e6f1ea7a5f8f94321bc4ffc15d553f0aa4
vpak1 <133903182+vpak1@users.noreply.github.com>
2026-06-29 19:25:26 -0400

    WIP: movement fixes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,12 +7,23 @@ public class PlayerMovement : MonoBehaviourPun
     public Rigidbody2D player1;
 
     public float moveSpeed = 5f;
+    public float climbSpeed = 4f;
+    public Animator animator;
+    
+    public SpriteRenderer spriteRenderer;
+    public LayerMask ladderLayer;
 
     private float horizontalMovement;
+    private bool climbInput;
+    private bool descendInput;
+    private bool isOnLadder;
+    private float defaultGravity;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultGravity = player1.gravityScale;
     }
 
     // Update is called once per frame
@@ -20,11 +31,61 @@ public class PlayerMovement : MonoBehaviourPun
     {
         if (!photonView.IsMine) return;
         player1.linearVelocity = new Vector2(horizontalMovement*moveSpeed, player1.linearVelocity.y);
+        
+        if (isOnLadder)
+        {
+            player1.gravityScale = 0f;
+
+            float verticalInput = 0f;
+            if (climbInput) verticalInput = 1f;
+            else if (descendInput) verticalInput = -1f;
+
+            player1.linearVelocity = new Vector2(player1.linearVelocity.x, verticalInput * climbSpeed);
+        }
+        else
+        {
+            player1.gravityScale = defaultGravity;
+        }
+        
+        animator.SetFloat("yVelocity", player1.linearVelocity.y);
+        animator.SetFloat("magnitude", Mathf.Abs(horizontalMovement));
+            
+        if (horizontalMovement > 0.01f)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (horizontalMovement < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         if (!photonView.IsMine) return;
         horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+    
+    public void Climb(InputAction.CallbackContext context)
+    {
+        if (!photonView.IsMine) return;
+        climbInput = context.ReadValueAsButton();
+    }
+    
+    public void Crouch(InputAction.CallbackContext context)
+    {
+        if (!photonView.IsMine) return;
+        descendInput = context.ReadValueAsButton();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (((1 << other.gameObject.layer) & ladderLayer) != 0)
+            isOnLadder = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (((1 << other.gameObject.layer) & ladderLayer) != 0)
+        {
+            isOnLadder = false;
+            player1.gravityScale = defaultGravity;
+        }
     }
 }
