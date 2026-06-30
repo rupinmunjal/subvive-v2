e30942158f976548d158e18eb83e22872ab841b8
vpak1 <133903182+vpak1@users.noreply.github.com>
2026-06-29 21:46:02 -0400

    Fix movement and animator setup

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,66 +1,92 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Photon.Pun;
-
+using Photon.Realtime;
 public class PlayerMovement : MonoBehaviourPun
 {
     public Rigidbody2D player1;
+
     public float moveSpeed = 5f;
-    public float climbSpeed = 3f;
+    public float climbSpeed = 4f;
+    public Animator animator;
+    
+    public SpriteRenderer spriteRenderer;
+    public LayerMask ladderLayer;
 
     private float horizontalMovement;
-    private bool isOnLadder = false;
-    private LadderZone currentLadder;
-    private Collider2D myCollider;
-
+    private bool climbInput;
+    private bool descendInput;
+    private bool isOnLadder;
+    private float defaultGravity;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        myCollider = GetComponent<Collider2D>();
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        defaultGravity = player1.gravityScale;
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (!photonView.IsMine) return;
-
-        player1.linearVelocity = new Vector2(horizontalMovement * moveSpeed, player1.linearVelocity.y);
-
+        player1.linearVelocity = new Vector2(horizontalMovement*moveSpeed, player1.linearVelocity.y);
+        
         if (isOnLadder)
         {
-            if (Input.GetKey(KeyCode.UpArrow))
-            {
-                player1.gravityScale = 0f;
-                player1.linearVelocity = new Vector2(player1.linearVelocity.x, climbSpeed);
-            }
-            else if (Input.GetKey(KeyCode.DownArrow))
-            {
-                player1.gravityScale = 0f;
-                player1.linearVelocity = new Vector2(player1.linearVelocity.x, -climbSpeed);
-                if (currentLadder != null)
-                    currentLadder.DisableFloorBelowFor(myCollider);
-            }
-            else
-            {
-                player1.gravityScale = 1f;
-            }
+            player1.gravityScale = 0f;
+
+            float verticalInput = 0f;
+            if (climbInput) verticalInput = 1f;
+            else if (descendInput) verticalInput = -1f;
+
+            player1.linearVelocity = new Vector2(player1.linearVelocity.x, verticalInput * climbSpeed);
         }
         else
         {
-            player1.gravityScale = 1f;
+            player1.gravityScale = defaultGravity;
         }
+        
+        animator.SetFloat("yVelocity", player1.linearVelocity.y);
+        animator.SetFloat("magnitude", Mathf.Abs(horizontalMovement));
+            
+        if (horizontalMovement > 0.01f)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (horizontalMovement < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         if (!photonView.IsMine) return;
         horizontalMovement = context.ReadValue<Vector2>().x;
+        Debug.Log("Move called, value: " + horizontalMovement);
+    }
+    
+    public void Climb(InputAction.CallbackContext context)
+    {
+        if (!photonView.IsMine) return;
+        climbInput = context.ReadValueAsButton();
+    }
+    
+    public void Crouch(InputAction.CallbackContext context)
+    {
+        if (!photonView.IsMine) return;
+        descendInput = context.ReadValueAsButton();
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (((1 << other.gameObject.layer) & ladderLayer) != 0)
+            isOnLadder = true;
     }
 
-    public void SetCurrentLadder(LadderZone ladder)
+    void OnTriggerExit2D(Collider2D other)
     {
-        currentLadder = ladder;
-        isOnLadder = ladder != null;
-
-        if (ladder == null)
-            player1.gravityScale = 1f;
+        if (((1 << other.gameObject.layer) & ladderLayer) != 0)
+        {
+            isOnLadder = false;
+            player1.gravityScale = defaultGravity;
+        }
     }
 }
